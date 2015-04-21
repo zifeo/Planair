9cee56c8f27c7cadf659d22790e028270dc40b17
Teo Stocco <teo.stocco@epfl.ch>
2015-04-21 13:18:14 +0200

    Add video libraries

---
+++ src/main/java/ch/epfl/planair/visual/Webcam.java
@@ -0,0 +1,38 @@
+package ch.epfl.planair.visual;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.video.Capture;
+
+// need for $PROCESSING_ROOT/modes/java/libraries/video/library
+public final class Webcam extends PApplet {
+
+	private Capture cam;
+	private PImage img;
+
+	public void setup() {
+
+		size(640, 480);
+		String[] cameras = Capture.list();
+		if (cameras.length == 0) {
+			println("There are no cameras available for capture.");
+			exit();
+		} else {
+			println("Available cameras:");
+			for (int i = 0; i < cameras.length; i++) {
+				println(cameras[i]);
+			}
+			cam = new Capture(this, cameras[0]);
+			cam.start();
+		}
+	}
+
+	public void draw() {
+		if (cam.available() == true) {
+			cam.read();
+		}
+		img = cam.get();
+		image(img, 0, 0);
+	}
+
+}
