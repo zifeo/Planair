60d7200347624e1538cc29e2af5d0283ed522436
Teo Stocco <teo.stocco@epfl.ch>
2015-04-21 11:30:10 +0200

    Implement sobel

--- src/main/java/ch/epfl/planair/visual/Sobel.java
+++ src/main/java/ch/epfl/planair/visual/Sobel.java
@@ -25,6 +25,19 @@ public final class Sobel extends PApplet {
 			{  9, 12,  9 }
 	};
 
+	private final static float[][] kernelH = {
+			{ 0,  1, 0 },
+			{ 0,  0, 0 },
+			{ 0, -1, 0 }
+	};
+
+	private final static float[][] kernelV = {
+			{ 0, 0,  0 },
+			{ 1, 0, -1 },
+			{ 0, 0,  0 }
+	};
+
+
 	public void setup() {
 		size(800, 600);
 		img = loadImage("board/board1.jpg");
@@ -36,7 +49,8 @@ public final class Sobel extends PApplet {
 		background(color(0, 0, 0));
 
 		// kernel1 or kernel2 or gaussianKernel
-		PImage result = convolute(img, gaussianKernel);
+		//PImage result = convolute(img, gaussianKernel);
+		PImage result = sobel(img);
 
 		image(result, 0, 0);
 	}
@@ -51,7 +65,7 @@ public final class Sobel extends PApplet {
 		float weight = 0;
 		for (float[] x: kernel) {
 			for (float f: x) {
-				weight += f;
+				weight += abs(f);
 			}
 		}
 		int margin = kernel.length / 2;
@@ -72,35 +86,41 @@ public final class Sobel extends PApplet {
 		return result;
 	}
 
-	/*public PImage sobel(PImage img) {
-		float[][] hKernel = { { 0, 1, 0 },
-				{ 0, 0, 0 },
-				{ 0, -1, 0 } };
-		float[][] vKernel = { { 0, 0, 0 },
-				{ 1, 0, -1 },
-				{ 0, 0, 0 } };
+	public PImage sobel(PImage source) {
 
-		PImage result = createImage(img.width, img.height, ALPHA);
-		for (int i = 0; i < img.width * img.height; i++) {
-			result.pixels[i] = color(0);
-		}
+		PImage result = createImage(source.width, source.height, ALPHA);
+
+		int margin = kernelH.length / 2;
 		float max = 0;
-		float[] buffer = new float[img.width * img.height];
+		float[][] buffer = new float[img.width][img.height];
 
-		for (int i = 0; i < result.width * result.height; i++) {
-			result.pixels[i];
-		}
+		for (int x = margin; x + margin < result.width; ++x) {
+			for (int y = margin; y + margin < result.height; ++y) {
 
-			for (int y = 2; y < img.height - 2; y++) {
-			for (int x = 2; x < img.width - 2; x++) {
-				if (buffer[y * img.width + x] > (int)(max * 0.3f)) {
-					result.pixels[y * img.width + x] = color(255);
-				} else {
-					result.pixels[y * img.width + x] = color(0);
+				float sumH = 0;
+				float sumV = 0;
+				for (int px = x - margin, sx = 0; px <= x + margin; ++px, ++sx) {
+					for (int py = y - margin, sy = 0; py <= y + margin; ++py, ++sy) {
+
+						int id = pixel(source, px, py);
+						sumH += brightness(source.pixels[id]) * kernelH[sx][sy];
+						sumV += brightness(source.pixels[id]) * kernelV[sx][sy];
+					}
+				}
+				buffer[x][y] = sqrt(sumH * sumH + sumV * sumV);
+				if (buffer[x][y] > max) {
+					max = buffer[x][y];
 				}
 			}
 		}
+
+		for (int x = margin; x + margin < result.width; ++x) {
+			for (int y = margin; y + margin < result.height; ++y) {
+				result.pixels[pixel(source, x, y)] = buffer[x][y] > 0.3 * max ? color(255): color(0);
+			}
+		}
+
 		return result;
-	}*/
+	}
 
 }
