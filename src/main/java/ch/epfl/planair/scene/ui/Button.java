f736650b33587a44bae15075814de1ded0296c8c
Teo Stocco <teo.stocco@epfl.ch>
2015-05-25 13:58:19 +0200

    Step 1

--- src/main/java/ch/epfl/planair/scene/ui/Button.java
+++ src/main/java/ch/epfl/planair/scene/ui/Button.java
@@ -1,76 +1,80 @@
 package ch.epfl.planair.scene.ui;
 
-import ch.epfl.planair.meta.Constants;
-import ch.epfl.planair.scene.ui.Action;
 import ch.epfl.planair.meta.Utils;
 import ch.epfl.planair.specs.Drawable;
-import processing.core.PApplet;
 import processing.core.PFont;
 import processing.core.PGraphics;
 import processing.core.PVector;
 
-public final class Button extends Drawable {
+public abstract class Button extends Drawable {
 
+	protected final PGraphics screen;
 	private Action callback;
-	private boolean active;
-	private final int x;
-	private final int y;
-	private final int width;
-	private final int height;
-	private final String text;
-	private final PFont font;
-	private int fill;
-	private int stroke;
-	private final PGraphics screen;
 
-	public Button(PGraphics screen, int x, int y, int width, int height, String text, Action callback) {
+	protected final int x;
+	protected final int y;
+	protected final int sx;
+	protected final int sy;
+	protected final int width;
+	protected final int height;
+	protected final PFont font;
+
+	private boolean active;
+
+	/**
+	 *
+	 * @param screen
+	 * @param x
+	 * @param y
+	 * @param sx x correction for mouse hover
+	 * @param sy y correction for mouse hover
+	 * @param width
+	 * @param height
+	 */
+	public Button(PGraphics screen, int x, int y, int sx, int sy, int width, int height) {
 		super(screen.parent, new PVector(x, y, 0));
 		this.screen = screen;
-		this.callback = callback;
 		this.active = true;
 		this.x = x;
 		this.y = y;
+		this.sx = sx;
+		this.sy = sy;
 		this.width = width;
 		this.height = height;
-		this.text = text.toUpperCase();
 		this.font = parent.createFont("fonts/SF-Archery-Black/SF_Archery_Black.ttf", 25);
-		this.stroke = Constants.COLOR1;
-		this.fill = Constants.COLORBG;
 	}
 
 	@Override
-	public void draw() {
-		screen.stroke(stroke);
-		screen.fill(fill);
-		screen.rect(x, y, width - 1, height - 1);
-		screen.fill(Constants.COLOR1);
-		screen.textFont(font);
-		screen.textAlign(parent.CENTER, parent.CENTER);
-		screen.text(text, x + width / 2, y + height / 2 - 2);
-	}
+	public abstract void draw();
 
 	public void disable() { active = false; }
 
 	public void enable() { active = false; }
 
+	public PFont font() {
+		return font;
+	}
+
+	public boolean active() {
+		return active;
+	}
+
 	public boolean hover() {
-		return Utils.in(0, parent.mouseX - (parent.width - Constants.MENU_WIDTH) / 2 - x, width) &&
-				Utils.in(0, parent.mouseY - parent.height / 4 - y, height);
+		return Utils.in(0, relatifX(), width) &&
+				Utils.in(0, relatifY(), height);
 	}
 
-	public void mousePressed() {
-		if (active && hover()) {
-			callback.run();
-		}
+	public int relatifX() {
+		return relatifX(parent.mouseX);
+	}
+	public int relatifX(int mx) {
+		return mx - parent.width / 2 - sx - x;
+	}
+	public int relatifY() {
+		return relatifY(parent.mouseY);
+	}
+	public int relatifY(int mx) {
+		return mx - parent.height / 2 - sy - y;
 	}
 
-	public void mouseReleased() {
-		//fill = Constants.COLORBG;
-	}
-
-	public void mouseMoved() {
-		if (active) {
-			stroke = hover() ? Constants.RED: Constants.COLOR1;
-		}
-	}
 }
