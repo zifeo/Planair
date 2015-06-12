a7c707b1d5c7cb833b40f55ff4bd2804478c6a1b
Timothée Lottaz <timozattol@gmail.com>
2015-06-12 15:26:08 +0200

    Change course name in comments

--- src/main/java/ch/epfl/planair/Planair.java
+++ src/main/java/ch/epfl/planair/Planair.java
@@ -17,7 +17,7 @@ import java.util.Map;
  * Planair main class.
  * Game loading, mode dependence setup, events forwarding and ticks.
  *
- * Visual computing class 2015
+ * Introduction to Visual computing class 2015
  * Contact: firstname.surname@epfl.ch
  *
  * @author Nicolas Casademont
